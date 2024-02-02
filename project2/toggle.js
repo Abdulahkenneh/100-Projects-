c671f9be0416926db598f5e9ca4dd4d6f4a00e85
Abdulahkenneh <abulayeekennedy@gmail.com>
2024-02-02 21:16:53 +0100

    Dark Mode project

---
+++ project2/toggle.js
@@ -0,0 +1,19 @@
+const toggle = document.getElementById("container");
+const body = document.querySelector("body");
+const span = document.querySelector("span")
+//toggle.style.background = "white"
+
+toggle.onclick = function() {
+  toggle.classList.toggle("active");
+  body.classList.toggle("active");
+  span.classList.toggle("active")
+}
+
+span.onclick = function(){
+  if (span.innerHTML==="OFF"){
+  span.innerHTML = "ON"
+  }
+  else {
+    span.innerHTML = "OFF"
+  }
+}
